ddacc1913f452a17e87c8f3836d9f63c1b1def9c
rubou.chen <rubou.chen@gmail.com>
2019-02-13 15:10:32 +0900

    add the hello application(spring boot).

---
+++ hello/src/main/java/co/jp/okcoin/studyspring/HelloApplication.java
@@ -0,0 +1,14 @@
+package co.jp.okcoin.studyspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloApplication.class, args);
+    }
+
+}
+
