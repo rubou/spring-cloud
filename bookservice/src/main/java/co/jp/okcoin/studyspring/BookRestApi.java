2ad1fe36a5efcf19975df4191b3637ff53d107c5
rubou.chen <rubou.chen@gmail.com>
2019-02-13 17:39:09 +0900

    update for mtg

--- bookservice/src/main/java/co/jp/okcoin/studyspring/BookRestApi.java
+++ bookservice/src/main/java/co/jp/okcoin/studyspring/BookRestApi.java
@@ -1,12 +1,14 @@
 package co.jp.okcoin.studyspring;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/book-service")
+@RefreshScope
 public class BookRestApi {
 
     @Autowired
